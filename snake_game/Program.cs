2c77c3e4ca7689b940b2019dcd181ded79646961
Daniel Asante-Otchere <danielotchere18@gmail.com>
2024-10-12 22:52:11 +0100

    added border

--- snake_game/Program.cs
+++ snake_game/Program.cs
@@ -2,3 +2,20 @@
 
 Coord gridDimensions = new Coord(50, 20);
 Coord snakePosition = new Coord(10, 0);
+
+for (int y = 0; y < gridDimensions.Y; y++)
+{
+    for (int x = 0; x < gridDimensions.X; x++)
+    {
+        if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+        {
+            Console.Write("#");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+
+    Console.WriteLine();
+}
