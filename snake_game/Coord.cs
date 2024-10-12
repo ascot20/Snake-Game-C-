0a793484e5ee580f27e27be17588d28bd463a26b
Daniel Asante-Otchere <danielotchere18@gmail.com>
2024-10-12 22:28:20 +0100

    Add project files.

---
+++ snake_game/Coord.cs
@@ -0,0 +1,19 @@
+﻿namespace snake_game
+{
+    class Coord
+    {
+        private int x;
+        private int y;
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+
+
+    }
+}
---
+++ snake_game/Program.cs
@@ -0,0 +1,4 @@
+﻿using snake_game;
+
+Coord gridDimensions = new Coord(50, 20);
+Coord snakePosition = new Coord(10, 0);
